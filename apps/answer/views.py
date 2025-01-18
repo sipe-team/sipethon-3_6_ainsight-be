44c17db6d385a445432edd60c2801201cff92f18
clogic <choiseungil29@gmail.com>
2025-01-18 14:49:43 +0900

    fix: page size 수정

--- apps/answer/views.py
+++ apps/answer/views.py
@@ -9,7 +9,7 @@ router = Router(tags=["Answer"])
 
 
 @router.get('/answers', response=list[UserAnswerSchema])
-@paginate(ContribPageNumberPagination, page_size=9)
+@paginate(ContribPageNumberPagination, page_size=4)
 def get_answers(request):
     answers = UserAnswer.objects.all()
     return answers
