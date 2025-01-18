650af980136108e9a93305417eed5d6186a3a4d6
clogic <choiseungil29@gmail.com>
2025-01-18 11:51:22 +0900

    feat: API 작성 및 pagination 적용

--- apps/answer/views.py
+++ apps/answer/views.py
@@ -1,12 +1,15 @@
 from ninja import Router
+from ninja.pagination import paginate
 
 from apps.answer.models import UserAnswer
 from apps.answer.schemas import UserAnswerSchema
+from contrib.django.ninja.pagination import ContribPageNumberPagination
 
 router = Router(tags=["Answer"])
 
 
 @router.get('/answers', response=list[UserAnswerSchema])
+@paginate(ContribPageNumberPagination, page_size=9)
 def get_answers(request):
     answers = UserAnswer.objects.all()
     return answers
