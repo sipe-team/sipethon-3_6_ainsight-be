77800d9c9688ef3c1c81f419a9187ac14a86070b
clogic <choiseungil29@gmail.com>
2025-01-20 20:51:45 +0900

    feat: settings 수정

--- django_project/settings/dev.py
+++ django_project/settings/dev.py
@@ -1,21 +1,27 @@
+import os
+
+from dotenv import load_dotenv
+
 from .base import *
 
 ENVIRONMENT = "dev"
 
 DEBUG = True
 
+load_dotenv()
+
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "postgres",
-        "HOST": "fly-0-nrt.pooler.supabase.com",
-        "USER": "postgres.aqzyuyngwfdahoeccreq",
-        "PASSWORD": "UbsziHCGbA8ASkO4",
-        "PORT": "5432",
+        "HOST": os.getenv('DB_HOST'),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "PORT": os.getenv('DB_PORT'),
     }
 }
 
-ALLOWED_HOSTS = ['ainsight-be.fly.dev', 'sipethon-3-6-ainsight-fe.vercel.app/']
+ALLOWED_HOSTS = ['ainsight-be.fly.dev', 'sipethon-3-6-ainsight-fe.vercel.app/', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ["https://*.fly.dev/", 'https://*.vercel.app/']
